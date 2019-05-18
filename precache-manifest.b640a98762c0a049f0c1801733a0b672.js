3039bca8a77b2773f0546d6cdbb032cecb2b9163
lcukerd <lcukerd@github.com>
2019-05-18 20:30:47 +0530

    Updates

---
+++ precache-manifest.b640a98762c0a049f0c1801733a0b672.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60734b01075fdc4b76343201d0acb2f0",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "58f741db63f77630dabb",
+    "url": "/Weather-React/static/js/2.4203e1e4.chunk.js"
+  },
+  {
+    "revision": "c8ced7fea6f1fc86bd10",
+    "url": "/Weather-React/static/js/main.0da02132.chunk.js"
+  },
+  {
+    "revision": "aed0707c7348b231f061",
+    "url": "/Weather-React/static/js/runtime~main.6dcfcbf7.js"
+  }
+]);
